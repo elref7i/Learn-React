dc2dd13cab9e94dfb761b81f71ed71eb69fd5501
Ahmed Refai <momo3956248@gmail.com>
2024-11-23 16:53:49 +0200

    RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-// import Home from './pages/Home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Signup from './pages/Signup/Signup';
-// import Login from './pages/Login/Login';
-// import Home from './pages/Home/Home';
-// import NotFound from './pages/Notfound/NotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
+import NotFound from './pages/Notfound/NotFound';
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: 'home', element: <Home /> },
+        { path: 'login', element: <Login /> },
+        { path: 'signup', element: <Signup /> },
+        { path: '*', element: <NotFound /> },
+      ],
+    },
+  ]);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="home" element={<Home />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />;
     </>
   );
 }
+
+// <>
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route path="home" element={<Home />} />
+//         <Route path="signup" element={<Signup />} />
+//         <Route path="login" element={<Login />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>
+// </>
